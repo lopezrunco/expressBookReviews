b245954babf72e323815e636f6f10eb8091616ce
viejo bucanero <lopezrunco@gmail.com>
2024-08-12 12:04:34 +0530

    Completed task 5: getbookreview

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -65,8 +65,12 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Get the ISBN from the params.
+  const isbn = req.params.isbn
+  // Get the book by ISBN.
+  let book = books[isbn]
+  // Return the reviews.
+  res.send(book.reviews)
 });
 
 module.exports.general = public_users;
