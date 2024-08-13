829ef24b60a543f59797c070b3be02a5d60d7696
viejo bucanero <lopezrunco@gmail.com>
2024-08-13 10:30:56 +0530

    Completed task 11: Convert getBookByIsbn to Promise/Callback.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -65,13 +65,20 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  // Get the ISBN from the params.
-  const isbn = req.params.isbn
-  // Get the book by ISBN.
-  let book = books[isbn]
+  const getBookByIsbn = new Promise((resolve, reject) => {
+    // Get the ISBN from the params.
+    const isbn = req.params.isbn
+    // Get the book by ISBN.
+    let book = books[isbn]
+    resolve(book)
+  })
 
-  res.send(book)
- });
+  getBookByIsbn.then(data => {
+    res.json(data)
+  }).catch(error => {
+    res.json({ "error": "Unable to get a book by ISBN." })
+  })
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
